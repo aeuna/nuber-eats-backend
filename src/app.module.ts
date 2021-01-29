23bfd75057af89599db5e402087101f7d8e2fd25
aeuna <4294psy@naver.com>
2021-01-29 21:16:18 +0900

    Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true, //어플리케이션 어디서나 config접근이 가능
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'euna',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
       synchronize: true, //데이터베이스를 모듈의 현재상태로 마이그레이션하겠다.
       logging: true, //데이터베이스에서 무슨일이 일어나는지 콘솔에 표시
     }),
