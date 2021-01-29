19ae1ed2f44b9f57f8773ca7928b2e5dc0be9e11
aeuna <4294psy@naver.com>
2021-01-29 20:10:48 +0900

    Add inputTypes and argumentTypes

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/restaurant-create.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
@@ -6,5 +7,9 @@ export class RestaurantResolver {
   @Query((returns) => [Restaurant]) // return type is for graphql
   myRestaurant(@Args('vegan') vegan: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
+    return true;
   }
 }
