13faac796e38e5bcbd6951b75f2b599c26bcf975
aeuna <4294psy@naver.com>
2021-01-29 21:05:24 +0900

    Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //어플리케이션 어디서나 config접근이 가능
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'euna',
+      database: 'nuber-eats',
+      synchronize: true, //데이터베이스를 모듈의 현재상태로 마이그레이션하겠다.
+      logging: true, //데이터베이스에서 무슨일이 일어나는지 콘솔에 표시
     }),
     RestaurantsModule,
   ],
