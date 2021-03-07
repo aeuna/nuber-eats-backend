5eb12f99a9a3ff6a51dd2428041fbcaf9535da5c
aeuna <4294psy@naver.com>
2021-03-07 21:03:38 +0900

    Generating JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { User } from './users/entities/user.entity';
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        SECRET_KEY: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot({
