e6abe05390fc44cde1ac2b50b58f379c877481e2
aeuna <4294psy@naver.com>
2021-03-16 23:12:51 +0900

    Add updateProfile

--- src/users/dtos/create-account.dto.ts
+++ src/users/dtos/create-account.dto.ts
@@ -1,5 +1,5 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
-import { MutationOutput } from 'src/common/dtos/ouput.dto';
+import { InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/ouput.dto';
 import { User } from '../entities/user.entity';
 
 @InputType()
@@ -10,4 +10,4 @@ export class CreateAccountInput extends PickType(User, [
 ]) {}
 
 @ObjectType()
-export class CreateAccountOutput extends MutationOutput {}
+export class CreateAccountOutput extends CoreOutput {}
