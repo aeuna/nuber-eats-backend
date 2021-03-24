da9c4650a6f55e4be2f77446463efb684cf24e71
aeuna <4294psy@naver.com>
2021-03-25 00:17:16 +0900

    Refactor mail part

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,34 +2,30 @@ import got from 'got';
 import * as FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { MailModuleOptions } from './mail.interfaces';
+import { EmailVar, MailModuleOptions } from './mail.interfaces';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    // this.sendEmail('testing', 'test');
-    this.sendEmail('testing', 'test')
-      .then(() => {
-        console.log('Message sent');
-      })
-      .catch((error) => {
-        console.log(error.response.body);
-      });
-  }
+  ) {}
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(
+    subject: string,
+    template: string,
+    emailVars: EmailVar[],
+  ) {
     const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append(
+      'from',
+      `Euna from Nuber Eats <mailgun@${this.options.domain}>`,
+    );
     form.append('to', `aheunlee0113@gmail.com`);
     form.append('subject', subject);
-    form.append('template', 'verify-email');
-    form.append('v:code', 'testtest');
-    form.append('v:username', 'euna');
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      {
+    form.append('template', template);
+    emailVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
+    try {
+      await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         method: 'POST',
         headers: {
           Authorization: `Basic ${Buffer.from(
@@ -37,8 +33,16 @@ export class MailService {
           ).toString('base64')}`,
         },
         body: form,
-      },
-    );
-    console.log(response.body);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Verify Your Email', 'verify-email', [
+      { key: 'code', value: code },
+      { key: 'username', value: email },
+    ]);
   }
 }
